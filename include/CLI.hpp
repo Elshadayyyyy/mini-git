27d4450b1f6fdc6717edd823ce4a87ec090d6518
narDae77 <nardosanten886@gmail.com>
2026-05-12 16:41:31 +0300

    added features on the CLI

--- include/CLI.hpp
+++ include/CLI.hpp
@@ -5,16 +5,20 @@
 
 #include"CommitHandler.hpp"
 #include"FileSystem.hpp"
-
+#include"Utils.hpp"
+#include "DatabaseManager.hpp"
 using namespace std;
 
 namespace CLI {
+    DatabaseManager db(".mgit/minigit.db");
+
     void printHelp(){
         cout<<"\n minigit- A Simple Version Control System \n";
         cout<<"Commands:\n";
         cout<<"minigit init     -initialize a new repository\n";
         cout<<"minigit commit -m\"msg\" -save a snapshot of your files\n";
         cout<<"minigit log      -view commit history\n";
+        cout<<"minigit restore <id>    -restore files from a commit\n"; 
         cout<<"minigit help     -show this menu\n";
         cout<<"\n";
     }
@@ -27,7 +31,8 @@ namespace CLI {
          string command = argv[1];
 
          if(command =="init"){
-            FileSystem::init();
+            FileSystem::initialize_folders(); 
+            CommitHandler::handleInit(db); 
             cout<<"intialized empty minigit repository.\n";
          }
          else if(command == "commit"){
@@ -40,17 +45,50 @@ namespace CLI {
         string flag  = argv[2];
         if (flag!="-m"){
             cout<<"Error unknown flag"<<flag<<endl;
-            cout<<"How to use: minigit commit -m\"your message\"n";
+            cout<<"How to use: minigit commit -m\"your message\"\n";
             return;
         }
 
         string message = argv[3];
-        CommitHandler::commit(message);
+        string commitID = Utils::generate_alphanumeric(8);
+        FileSystem::snapshot(commitID);      
+        CommitHandler::Commit commit;
+        commit.id        = commitID;
+        commit.message   = message;
+        commit.author    = "Unknown";
+        commit.timestamp = Utils::get_db_iso_string(); 
         
-       }else if(command=="log"){
-        CommitHandler::log();
+        CommitResponse result = CommitHandler::handleCommit(db, commit);
+
+            if(result.success){
+                cout<<"Snapshot saved. Commit ID: "<<commitID<<"\n";
+            }else{
+                cout<<"Error: "<<result.message<<"\n";
+            }
+         }else if(command=="log"){
+           
+          CommitHandler::handleLog(db); 
        
-       }else if(command=="Help"){
+        }else if(command=="restore"){ 
+
+            if(argc < 3){
+                cout<<"Error: restore requires a commit ID.\n";
+                cout<<"How to use: minigit restore <commitID>\n";
+                return;
+            } 
+        string commitID = argv[2];
+
+        CommitHandler::Commit found;
+            if(!CommitHandler::fetchCommitById(db, commitID, found)){
+                cout<<"Error: commit ID '"<<commitID<<"' not found.\n";
+                return;
+            }
+        
+        FileSystem::restore(commitID);
+            cout<<"Restored files from commit: "<<commitID<<"\n";
+
+        
+        }else if(command == "help"){ 
         printHelp();
 
        }else{
